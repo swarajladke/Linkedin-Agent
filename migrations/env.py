885df6fca1e90fb2506fda39fcdf47fcf769a44f
Swaraj Ladke <swarajladke20@gmail.com>
2026-09-24 13:27:16 +0530

    fix(alembic): add logging configuration sections to alembic.ini

--- migrations/env.py
+++ migrations/env.py
@@ -15,9 +15,11 @@ from pilot.db.session import Base
 config = context.config
 
 # Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    try:
+        fileConfig(config.config_file_name, disable_existing_loggers=False)
+    except Exception:
+        pass
 
 # add your model's MetaData object here
 # for 'autogenerate' support
